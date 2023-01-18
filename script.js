722a4ea0f5e36840ea378ac17f848288f3b94830
Christopher Fuchs <fuchs-christopher@gmx.de>
2023-01-18 18:30:24 +0100

    Adedd work on final vision

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let pokemons = [];
 let offset = 0;
 
 async function loadPokemon() {
+    loadingNow();
     let allPokemon3 = document.getElementById('allPokemon');
     let load = document.getElementById('isLoading');
     let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`;
@@ -12,7 +13,6 @@ async function loadPokemon() {
         let individualResponse = await fetch(allPokemon);
         currentPokemon = await individualResponse.json();
         pokemons.push(currentPokemon);
-        loadingNow();
     }
     renderAllPokemons(pokemons);
     load.classList.add('d-none');
@@ -40,18 +40,18 @@ function checkButton(pokemons, i) {
 }
 
 
-function loadMorePokemon() {
+function loadMorePokemon(pokemons, i) {
     let container = document.getElementById('allPokemon2');
     container.innerHTML = '';
     offset += 20;
     loadPokemon();
+    
 }
 
 function loadingNow() {
     let allPokemon3 = document.getElementById('allPokemon');
     allPokemon3.classList.add('d-none');
     document.getElementById('loadBtn').classList.add('d-none');
-
     let load = document.getElementById('isLoading');
     load.classList.remove('d-none');
 }
@@ -129,134 +129,74 @@ function checkSecondTypeforAll(pokemons, i) {
 
 
 function showAbout(i) {
+    clearTheFields();
+    let activeLink = 'types'
     let aboutPokemon = document.getElementById('aboutField1');
     aboutPokemon.innerHTML = '';
+    removeHover();
     let currentPokemon = pokemons[i];
     aboutPokemon.innerHTML = showAbout2(currentPokemon);
-
-    clearTheFields1();
-    addAndRemoveHover1();
-}
-
-
-function addAndRemoveHover1() {
-    let linksabout = document.getElementById('linkAbout');
-    linksabout.classList.add('hoverKeep');
-    let linkBaseStats = document.getElementById('linkBaseStats');
-    linkBaseStats.classList.remove('hoverKeep');
-    let evolutionField2 = document.getElementById('linkEvolution');
-    evolutionField2.classList.remove('hoverKeep');
-    let movesField2 = document.getElementById('linkMoves');
-    movesField2.classList.remove('hoverKeep');
-}
-
-
-function clearTheFields1() {
-    let baseStats = document.getElementById('baseStatsField1');
-    baseStats.innerHTML = '';
-    let evolutionField1 = document.getElementById('evolutionField1');
-    evolutionField1.innerHTML = '';
-    let movesField1 = document.getElementById('movesField1');
-    movesField1.innerHTML = '';
+    addHoverEffect(activeLink);
 }
 
 
 function showBaseStats(i) {
+    clearTheFields();
+    let activeLink = 'stats'
     let baseStats = document.getElementById('baseStatsField1');
     baseStats.innerHTML = '';
+    removeHover();
     let currentPokemon = pokemons[i];
     baseStats.innerHTML = baseStatsAndProgressbar(currentPokemon);
-
-    clearTheFields2();
-    addAndRemoveHover2();
-}
-
-
-function addAndRemoveHover2() {
-    let linkBaseStats = document.getElementById('linkBaseStats');
-    linkBaseStats.classList.add('hoverKeep');
-    let linksabout = document.getElementById('linkAbout');
-    linksabout.classList.remove('hoverKeep');
-    let evolutionField2 = document.getElementById('linkEvolution');
-    evolutionField2.classList.remove('hoverKeep');
-    let movesField2 = document.getElementById('linkMoves');
-    movesField2.classList.remove('hoverKeep');
-}
-
-function clearTheFields2() {
-    let aboutPokemon = document.getElementById('aboutField1');
-    aboutPokemon.innerHTML = '';
-    let evolutionField1 = document.getElementById('evolutionField1');
-    evolutionField1.innerHTML = '';
-    let movesField1 = document.getElementById('movesField1');
-    movesField1.innerHTML = '';
+    addHoverEffect(activeLink);
 }
 
 
 function showEvolution(i) {
+    clearTheFields();
+    let activeLink = 'base_experience'
     let evolutionField1 = document.getElementById('evolutionField1');
     evolutionField1.innerHTML = '';
+    removeHover();
     let currentPokemon = pokemons[i];
     evolutionField1.innerHTML = evolutionExample(currentPokemon);
-
-    clearTheFields3();
-    addAndRemoveHover3();
-}
-
-
-function addAndRemoveHover3() {
-    let linksabout = document.getElementById('linkAbout');
-    linksabout.classList.remove('hoverKeep');
-    let linkBaseStats = document.getElementById('linkBaseStats');
-    linkBaseStats.classList.remove('hoverKeep');
-    let evolutionField2 = document.getElementById('linkEvolution');
-    evolutionField2.classList.add('hoverKeep');
-    let movesField2 = document.getElementById('linkMoves');
-    movesField2.classList.remove('hoverKeep');
-}
-
-
-function clearTheFields3() {
-    let aboutPokemon = document.getElementById('aboutField1');
-    aboutPokemon.innerHTML = '';
-    let baseStats = document.getElementById('baseStatsField1');
-    baseStats.innerHTML = '';
-    let movesField1 = document.getElementById('movesField1');
-    movesField1.innerHTML = '';
+    addHoverEffect(activeLink);
 }
 
 
 function showMoves(i) {
+    clearTheFields();
+    let activeLink = 'moves'
     let movesField1 = document.getElementById('movesField1');
     movesField1.innerHTML = '';
+    removeHover();
     let currentPokemon = pokemons[i];
-
     movesField1.innerHTML = showMoves2(currentPokemon);
-
-    clearTheFields4();
-    addAndRemoveHover4();
+    addHoverEffect(activeLink);
 }
 
 
-function addAndRemoveHover4() {
-    let movesField2 = document.getElementById('linkMoves');
-    movesField2.classList.add('hoverKeep');
-    let linksabout = document.getElementById('linkAbout');
-    linksabout.classList.remove('hoverKeep');
-    let linkBaseStats = document.getElementById('linkBaseStats');
-    linkBaseStats.classList.remove('hoverKeep');
-    let evolutionField2 = document.getElementById('linkEvolution');
-    evolutionField2.classList.remove('hoverKeep');
-}
-
-
-function clearTheFields4() {
-    let aboutPokemon = document.getElementById('aboutField1');
-    aboutPokemon.innerHTML = '';
+function clearTheFields() {
     let baseStats = document.getElementById('baseStatsField1');
     baseStats.innerHTML = '';
     let evolutionField1 = document.getElementById('evolutionField1');
     evolutionField1.innerHTML = '';
+    let movesField1 = document.getElementById('movesField1');
+    movesField1.innerHTML = '';
+    let aboutPokemon = document.getElementById('aboutField1');
+    aboutPokemon.innerHTML = '';
+}
+
+
+function removeHover() {
+    let linksabout = document.getElementById(`linkAbout`);
+    linksabout.classList.remove('hoverKeep');
+    let linkBaseStats = document.getElementById(`linkBaseStats`);
+    linkBaseStats.classList.remove('hoverKeep');
+    let evolutionField2 = document.getElementById(`linkEvolution`);
+    evolutionField2.classList.remove('hoverKeep');
+    let movesField2 = document.getElementById(`linkMoves`);
+    movesField2.classList.remove('hoverKeep');
 }
 
 
@@ -270,7 +210,24 @@ function filterNames() {
         if (name.toLowerCase().includes(search)) {
             container.innerHTML += searchResult(pokemons, i)
             checkTypeColor(pokemons, i);
+            checkSecondTypeforAll(pokemons, i);
         }
     }
-    
+}
+
+
+function addHoverEffect(clickedMenu) {
+    if (clickedMenu == 'types') {
+        let linksabout = document.getElementById(`linkAbout`);
+        linksabout.classList.add('hoverKeep');
+    } if (clickedMenu == 'stats') {
+        let linkBaseStats = document.getElementById(`linkBaseStats`);
+        linkBaseStats.classList.add('hoverKeep');
+    } if (clickedMenu == 'base_experience') {
+        let evolutionField2 = document.getElementById(`linkEvolution`);
+        evolutionField2.classList.add('hoverKeep');
+    } if (clickedMenu == 'moves') {
+        let movesField2 = document.getElementById(`linkMoves`);
+        movesField2.classList.add('hoverKeep');
+    }
 }
